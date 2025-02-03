c1ebbcf05817446b9572a429c7b0dc7c67faeded
evgenii <kartoxa28@gmail.com>
2025-02-03 16:36:36 +0300

    gde

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="wrapper">
       <Header />
-      <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
