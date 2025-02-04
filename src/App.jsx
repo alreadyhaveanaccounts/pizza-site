bad01c24759d648168d1a72d18705c82fa142d61
evgenii <kartoxa28@gmail.com>
2025-02-04 17:36:20 +0300

    6 lsn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import { Categories } from "./components/Categories";
 import { Header } from "./components/Header";
 import { PizzaBlock } from "./components/PizzaBlock";
 import { Sort } from "./components/Sort";
 import "./scss/app.scss";
-import { pizzas } from "./components/assets/pizzaz";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  useEffect(() => {
+    fetch("https://67a201eb409de5ed5253ea27.mockapi.io/items")
+      .then((res) => res.json())
+      .then((pizzaArr) => setPizzas(pizzaArr));
+  }, []);
+
+  console.log(pizzas);
+
   return (
     <div className="wrapper">
       <Header />
